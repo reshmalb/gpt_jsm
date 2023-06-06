f8315aa80dbe42cff42892c4ae96a7d866f365e6
reshmalb <reshma.lb27@gmail.com>
2023-06-06 23:14:00 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Article,Navbar,Feature,Brand,CTA } from './Components/indexcomponent'
 import { Blog,Features,Footer,Header,Possibility,WhatGpt3 } from './container/indexcontainer'
-
+import './App.css'
 const App = () => {
   return (
     <div className='App'>
-      <div className='gradient_bg'>
+      <div className='gradient__bg'>
         <Navbar/>
         <Header/>
         
--- src/Components/navbar/Navbar.js
+++ src/Components/navbar/Navbar.js
@@ -1,10 +1,32 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../../asset/logo.svg'
 
 const Navbar = () => {
   return (
-    <div>
-Navbar
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-links_logo'> 
+          <img src={logo} alt="logo"></img>
+        </div>
+        <div className='gpt3__navbar-menu_container'>
+          <p><a href="#home">Home</a></p>
+          <p><a href="#whatisGPT">What is GPT?</a></p>
+          <p><a href="#possibility">Possibilities</a></p>
+          <p><a href="#features">Features</a></p>
+          <p><a href="#blog">Blog</a></p>
+
+        </div>
+        </div>
+        <div className='gpt3__navbar-sign'>
+          <p>Sign in</p>
+          <button type="button"> SignUp
+
+          </button>
+
+        
+
+      </div>
     </div>
   )
 }
